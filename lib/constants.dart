dce2d0cbc616520f38246d13632be488ae1038da
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-11-09 21:50:57 +0100

    Genres constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,136 +1,65 @@
-//SOON TM
+enum MovieGenres {
+  Action(28),
+  Adventure(12),
+  Animation(16),
+  Comedy(35),
+  Crime(80),
+  Documentary(99),
+  Drama(18),
+  Family(10751),
+  Fantasy(14),
+  History(36),
+  Horror(27),
+  Music(10402),
+  Mystery(9648),
+  Romance(10749),
+  ScienceFiction(878),
+  TVMovie(10770),
+  Thriller(53),
+  War(10752),
+  Western(37),
+  None(0);
 
-//const Text("Trending", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//SizedBox(
-//  height: 200,
-//  child: FutureBuilder<Map<String, dynamic>>(
-//    future: _trendingFuture,
-//    builder: (context, snapshot) {
-//      if (snapshot.connectionState == ConnectionState.waiting) {
-//        return const Center(child: CircularProgressIndicator());
-//      } else if (snapshot.hasError) {
-//        return Center(child: Text('Error: ${snapshot.error}'));
-//      } else if (!snapshot.hasData || snapshot.data!['results'] == null) {
-//        return const Center(child: Text('No data available'));
-//      } else {
-//        final movies = snapshot.data!['results'] as List<dynamic>;
-//        movies.sort((a, b) => b['vote_average'].compareTo(a['vote_average']));
-//        return ListView.builder(
-//          itemBuilder: (context, index) {
-//            final movie = movies[index];
-//            return MediaTile(
-//              mediaTitle: _onTVPage ? movie['name'] : movie['title'],
-//              mediaPoster: movie['poster_path'],
-//              mediaRating: movie['vote_average'].toDouble(),
-//            );
-//          },
-//          itemCount: movies.length,
-//          scrollDirection: Axis.horizontal,
-//        );
-//      }
-//    },
-//  ),
-//),
-//if(_onTVPage)
-//  ...[
-//const Text("Returning this week", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//SizedBox(
-//  height: 200,
-//  child: FutureBuilder<Map<String, dynamic>>(
-//    future: _returningFuture,
-//    builder: (context, snapshot) {
-//      if (snapshot.connectionState == ConnectionState.waiting) {
-//        return const Center(child: CircularProgressIndicator());
-//      } else if (snapshot.hasError) {
-//        return Center(child: Text('Error: ${snapshot.error}'));
-//      } else if (!snapshot.hasData || snapshot.data!['results'] == null) {
-//        return const Center(child: Text('No data available'));
-//      } else {
-//        final movies = snapshot.data!['results'] as List<dynamic>;
-//        movies.sort((a, b) => b['vote_average'].compareTo(a['vote_average']));
-//        return ListView.builder(
-//          itemBuilder: (context, index) {
-//            final movie = movies[index];
-//            return MediaTile(
-//              mediaTitle: _onTVPage ? movie['name'] : movie['title'],
-//              mediaPoster: movie['poster_path'],
-//              mediaRating: movie['vote_average'].toDouble(),
-//            );
-//          },
-//          itemCount: movies.length,
-//          scrollDirection: Axis.horizontal,
-//        );
-//      }
-//    },
-//  ),
-//)],
-//const Text("New releases in October", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//SizedBox(
-//  height: 200,
-//  child: FutureBuilder<Map<String, dynamic>>(
-//    future: _fetchTrending(_onTVPage, null, '2024-10-01', '2024-10-31', null, null,'2024-10-01', '2024-10-31'),
-//    builder: (context, snapshot) {
-//      if (snapshot.connectionState == ConnectionState.waiting) {
-//        return const Center(child: CircularProgressIndicator());
-//      } else if (snapshot.hasError) {
-//        return Center(child: Text('Error: ${snapshot.error}'));
-//      } else if (!snapshot.hasData || snapshot.data!['results'] == null) {
-//        return const Center(child: Text('No data available'));
-//      } else {
-//        final movies = snapshot.data!['results'] as List<dynamic>;
-//        movies.sort((a, b) => b['vote_average'].compareTo(a['vote_average']));
-//        return ListView.builder(
-//          itemBuilder: (context, index) {
-//            final movie = movies[index];
-//            return MediaTile(
-//              mediaTitle: _onTVPage ? movie['name'] : movie['title'],
-//              mediaPoster: movie['poster_path'],
-//              mediaRating: movie['vote_average'].toDouble(),
-//            );
-//          },
-//          itemCount: movies.length,
-//          scrollDirection: Axis.horizontal,
-//        );
-//      }
-//    },
-//  ),
-//),
-//const Text("All time popular", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//SizedBox(
-//  height: 200,
-//  child: FutureBuilder<Map<String, dynamic>>(
-//    future: _fetchTrending(_onTVPage, null, null, null, null, null, null, null),
-//    builder: (context, snapshot) {
-//      if (snapshot.connectionState == ConnectionState.waiting) {
-//        return const Center(child: CircularProgressIndicator());
-//      } else if (snapshot.hasError) {
-//        return Center(child: Text('Error: ${snapshot.error}'));
-//      } else if (!snapshot.hasData || snapshot.data!['results'] == null) {
-//        return const Center(child: Text('No data available'));
-//      } else {
-//        final movies = snapshot.data!['results'] as List<dynamic>;
-//        movies.sort((a, b) => b['vote_average'].compareTo(a['vote_average']));
-//        return ListView.builder(
-//          itemBuilder: (context, index) {
-//            final movie = movies[index];
-//            return MediaTile(
-//              mediaTitle: _onTVPage ? movie['name'] : movie['title'],
-//              mediaPoster: movie['poster_path'],
-//              mediaRating: movie['vote_average'].toDouble(),
-//            );
-//          },
-//          itemCount: movies.length,
-//          scrollDirection: Axis.horizontal,
-//        );
-//      }
-//    },
-//  ),
-//),
-//const Text("Action", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//const Text("Drama", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//const Text("Horror", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//const Text("Anime", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//const Text("Adventure", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//const Text("Comedy", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//const Text("Crime", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-//const Text("Science Fiction", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+  final int id;
+
+  const MovieGenres(this.id);
+}
+
+enum SerieGenres {
+  ActionAdventure(10759),
+  Animation(16),
+  Comedy(35),
+  Crime(80),
+  Documentary(99),
+  Drama(18),
+  Family(10751),
+  Kids(10762),
+  Mystery(9648),
+  News(10763),
+  Reality(10764),
+  SciFiFantasy(10765),
+  Soap(10766),
+  Talk(10767),
+  WarPolitics(10768),
+  Western(37),
+  None(0);
+
+  final int id;
+
+  const SerieGenres(this.id);
+}
+
+List<String> months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
